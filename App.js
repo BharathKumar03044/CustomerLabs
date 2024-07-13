94e03e330af018b6a1aa98a4d75138123edb5bf0
BharathKumar03044 <kumarbharath560@gmail.com>
2024-07-13 17:41:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Segment from "./Savesegment/SaveSegment";
+const App = ()=>  {
+  
+    return ( <> 
+   
+ <h1>This Is Segment</h1>
+    <Segment/>
+    </>
+      
+    );
+  }
+  
+  export default App;
